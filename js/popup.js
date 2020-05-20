92ef5173014bfef6690b0ea42f40643ff605c510
meetm <meetmalde@gmail.com>
2020-05-20 17:09:47 +0530

    Set cookie for custom domain and display on popup

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,28 @@
-// var LNR_DOMAIN = "http://localhost";
-var LNR_DOMAIN = "https://www.khanacademy.org/";
+var LNR_DOMAIN = "http://localhost:3000";
+// var LNR_DOMAIN = "https://www.khanacademy.org/";
+
+var TEN_DAYS_IN_SECONDS = 864000;
 
 $(function () {
+  addCookie(LNR_DOMAIN, "test", "testValue");
+  showCookies(LNR_DOMAIN);
+});
+
+function addCookie(url, name, value) {
+  var current_time_in_epoch = new Date().getTime() / 1000;
+  chrome.cookies.set({
+    url: url,
+    name: name,
+    value: value,
+    expirationDate: current_time_in_epoch + TEN_DAYS_IN_SECONDS,
+  });
+}
+
+function showCookies(url) {
   chrome.tabs.getSelected(undefined, function (tab) {
     chrome.cookies.getAll(
       {
-        url: LNR_DOMAIN,
+        url: url,
       },
       function (cookies) {
         for (var i = 0; i < cookies.length; i++) {
@@ -20,4 +37,4 @@ $(function () {
       }
     );
   });
-});
+}
